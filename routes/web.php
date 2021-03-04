a8d66146f36e16faab3a45e41538ea454dda4a8d
hayaka.yokoo <littgle.manahaya@gmial.com>
2021-03-04 23:09:45 +0900

    CRUD tasks

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'TasksController@index');
+
+Route::resource('tasks', 'TasksController');
+
+/*
+// CRUD
+// タスクの個別詳細ページ表示
+Route::get('tasks/{id}', 'TasksController@show');
+// タスクの新規作成を処理
+Route::post('tasks', 'TasksController@store');
+// タスクの更新処理
+Route::put('tasks/{id}', 'TasksController@update');
+
+//index: showの補助ページ
+Route::get('tasks', 'TasksController@index')->name('task.index');
+// create: 新規作成用のフォームページ
+Route::get('tasks/create', 'TasksController@create')->name(tasks.create);
+// edit: 更新用のフォームページ
+Route::get('messages/{id}/edit', 'MessagesController@edit')->name(tasks.edit);
+*/
